d968d7076efbae56f865a35340fcddd0db1245bc
Advaith Anoop <advaith.anoop.1@mb332172.ad.pisd.edu>
2025-09-25 18:48:58 -0500

    4 motors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,13 +8,15 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystem.Motor;
 
 public class RobotContainer {
   public Motor motor1 = new Motor(new TalonFX(5));
+  public Motor motor2 = new Motor(new TalonFX(6));
+  public Motor motor3 = new Motor(new TalonFX(7));
+  public Motor motor4 = new Motor(new TalonFX(8));
   public CommandXboxController control = new CommandXboxController(0);
 
   public RobotContainer() {
@@ -22,11 +24,17 @@ public class RobotContainer {
   }
 
   public void onInitialize() {
-  motor1.setDefaultCommand(new RunCommand(() -> motor1.runMotor(0), motor1));
+    motor1.setDefaultCommand(new RunCommand(() -> motor1.runMotor(0), motor1));
+    motor2.setDefaultCommand(new RunCommand(() -> motor2.runMotor(0), motor2));
+    motor3.setDefaultCommand(new RunCommand(() -> motor3.runMotor(0), motor3));
+    motor4.setDefaultCommand(new RunCommand(() -> motor4.runMotor(0), motor4));
   }
 
   private void configureBindings() {
     control.a().whileTrue(new RunCommand(() -> motor1.runMotor(0.5)));
+    control.a().whileTrue(new RunCommand(() -> motor2.runMotor(0.5)));
+    control.a().whileTrue(new RunCommand(() -> motor3.runMotor(0.5)));
+    control.a().whileTrue(new RunCommand(() -> motor4.runMotor(0.5)));
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/subsystem/Motor.java
+++ src/main/java/frc/robot/subsystem/Motor.java
@@ -6,13 +6,23 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Motor extends SubsystemBase{
     public TalonFX motor1;
+    public TalonFX motor2;
+    public TalonFX motor3;
+    public TalonFX motor4;
+    
     public TalonFXConfiguration motorOneConfig;
 
     public Motor(TalonFX talon) {
         this.motor1 = talon;
+        this.motor2 = talon;
+        this.motor3 = talon;
+        this.motor4 = talon;
     }
 
     public void runMotor(double speed) {
         motor1.set(speed);
+        motor2.set(speed);
+        motor3.set(speed);
+        motor4.set(speed);
     }
 }
